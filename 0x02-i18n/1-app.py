cf162992ac2dad5844b601d3a47f50fc9f64ec57
Shamainb <shamainbhengesa60@gmail.com>
2024-05-28 22:39:01 +0200

    Basic Flask app

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -5,19 +5,24 @@ Install the Babel Flask
 from flask import Flask
 from flask_babel import Babel
 
+
 class Config:
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
+
 babel = Babel(app)
+
 
 @app.route('/')
 def index() -> str:
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
